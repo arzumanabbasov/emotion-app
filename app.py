bc6376dc88c688e4e5dc74509f045d92926695b9
Arzuman Abbasov <106863886+arzumanabbasov@users.noreply.github.com>
2024-05-29 03:21:46 +0400

    Initial Commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # app.py
 from flask import Flask, request, render_template
 from bilstm import predict_emotion
+import os
 
 app = Flask(__name__)
 
@@ -14,5 +15,9 @@ def index():
     return render_template('index.html', emotion=emotion)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+
+    is_prod = os.environ.get('RAILWAY_ENVIRONMENT_NAME') is not None
+
+    app.run(host='0.0.0.0', port=port, debug=not is_prod)
